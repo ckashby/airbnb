99d0cd046034462bda7419e66a5f365fa3e6ef80
ckashby <clay.ashby@gmail.com>
2018-05-11 12:24:13 -0700

    installed devise and ffaker

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+  	# it "has a valid factory" do
+  	# 	expect(FactoryBot.create(:member)).to be_valid
+  	# end
+  	it "has valid attributes" do
+  		expect(5).to eq(5)
+  	end
+  		
+  	end
 end
